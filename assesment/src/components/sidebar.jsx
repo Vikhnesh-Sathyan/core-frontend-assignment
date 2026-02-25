2a4e10ffcacd51e917930061ad6e96e1a417a9b1
Vikhnesh-Sathyan <Vikhneshsathyan007@gmail.com>
2026-02-25 20:15:18 +0530

    Modify sidebar

--- assesment/src/components/sidebar.jsx
+++ assesment/src/components/sidebar.jsx
@@ -1,7 +1,14 @@
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>Workspace</h2>
+      <h2 className="workspace-title">Workspace</h2>
+
+      <ul className="sidebar-menu">
+        <li className="menu-item active">People</li>
+        <li className="menu-item">Projects</li>
+        <li className="menu-item">Calendar</li>
+        <li className="menu-item">Reports</li>
+      </ul>
     </div>
   )
 }
