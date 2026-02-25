69b3ae76e659385d47ad48a68b5d200b0821ed44
Vikhnesh-Sathyan <Vikhneshsathyan007@gmail.com>
2026-02-25 20:33:01 +0530

    Modify Sidebar

--- assesment/src/components/sidebar.jsx
+++ assesment/src/components/sidebar.jsx
@@ -4,10 +4,14 @@ function Sidebar() {
       <h2 className="workspace-title">Workspace</h2>
 
       <ul className="sidebar-menu">
-        <li className="menu-item active">People</li>
-        <li className="menu-item">Projects</li>
-        <li className="menu-item">Calendar</li>
-        <li className="menu-item">Reports</li>
+        <li className="menu-item active">Home</li>
+        <li className="menu-item active">My Info</li>
+        <li className="menu-item">People</li>
+        <li className="menu-item">Team Management</li>
+        <li className="menu-item">Project Setup</li>
+        <li className="menu-item">Hiring</li>
+        <li className="menu-item">Report</li>
+                
       </ul>
     </div>
   )
